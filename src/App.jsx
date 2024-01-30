0686b0aeb82e3852dfcd8c8bbcc033574d06210a
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-01-30 12:01:24 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css';
 import LikeImage from './components/LikeImage';
 import LikePost from './components/LikePost';
+import Wrapper from './RENDER/Wrapper';
 
 function App() {
   return (
     <div>
-      <h3>Some Blog</h3>
+      <h3>HOC</h3>
       <div className='buttons'>
-        <LikePost/>
         <LikeImage/>
+        <LikePost />
+      </div>
+      <h3>Some Render</h3>
+      <div className='buttons'>
+        <Wrapper render={(counter, handleIncreament)=>{
+          return <LikeImage likeImageCounter = {counter} handleLikeImageCount = {handleIncreament}/>
+        }}/>
+        <Wrapper render={(counter, handleIncreament)=>{
+          return <LikePost likePostCounter = {counter} handleLikePostCount = {handleIncreament}/>
+        }}
+/>
       </div>
     </div>
   );
